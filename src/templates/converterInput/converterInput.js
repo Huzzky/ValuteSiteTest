249f4c9ab85bd6b4f803e1241478f07617e43ea7
Huskey <vladislav.bychkov01@gmail.com>
2020-07-19 00:00:19 +0300

    master

--- src/templates/converterInput/converterInput.js
+++ src/templates/converterInput/converterInput.js
@@ -6,7 +6,7 @@ class ConverterInput extends React.Component {
         this.state = {
             inputUSDLoaded: false,
             inputEULoaded: false,
-            inputUAHLoaded: false;
+            inputUAHLoaded: false,
 
         }
         this.btnUSD = this.btnUSD.bind(this);
@@ -15,16 +15,33 @@ class ConverterInput extends React.Component {
     }
     
     btnUAH() {
-
+        this.setState({
+            inputUSDLoaded: false,
+            inputEULoaded: false,
+            inputUAHLoaded: true,   
+        });
+        console.log(this.state.inputUSDLoaded, this.state.inputEULoaded, this.state.inputUAHLoaded);
     }
 
     btnEU() {
-
+        this.setState({
+            inputUSDLoaded: false,
+            inputEULoaded: true,
+            inputUAHLoaded: false,   
+        });
+        console.log(this.state.inputUSDLoaded, this.state.inputEULoaded, this.state.inputUAHLoaded);
     }
 
     btnUSD() {
-
+        this.setState({
+            inputUSDLoaded: true,
+            inputEULoaded: false,
+            inputUAHLoaded: false,   
+        });
+        console.log(this.state.inputUSDLoaded, this.state.inputEULoaded, this.state.inputUAHLoaded);
     }
+    
+
     
     componentDidMount() {
         fetch('https://www.cbr-xml-daily.ru/daily_json.js') // api от ЦБ РФ
@@ -45,15 +62,89 @@ class ConverterInput extends React.Component {
     }
 
     render() {
-        is
-        return(
-            <div>
-                <button onClick={}>USD</button>
-                <button>EU</button>
-                <button>UAH</button>
+        const {inputEULoaded, inputUAHLoaded, inputUSDLoaded, isLoaded} = this.state;
+        if (!isLoaded) {
+            return(<div>
+                <h1>Загрузка</h1>
+            </div>)
+        }
+        else if (inputEULoaded===true && inputUSDLoaded===false && inputUAHLoaded===false){
+            return(<div>
+                <BtnUSDValute onClick={this.btnUSD}/>
+                <BtnEUValute onClick={this.btnEU}/>
+                <BtnUAHValute onClick={this.btnUAH}/>
+                <h3>Введи сколько тебе нужно ЕВРО перевести в рубли</h3>
+                <form action="">
+                    <input type="text"/>
+                </form>
+            </div>)
+        }
+        else if (inputEULoaded===false && inputUAHLoaded===false && inputUSDLoaded===false) {
+            return <div>
+                <BtnUSDValute onClick={this.btnUSD}/>
+                <BtnEUValute onClick={this.btnEU}/>
+                <BtnUAHValute onClick={this.btnUAH}/>
+                <h1>Выбери валюту</h1>
             </div>
-        )
+        }
+        else if (inputEULoaded===true || inputUAHLoaded===false || inputUSDLoaded===true) {
+            return(<div>
+                <BtnUSDValute onClick={this.btnUSD}/>
+                <BtnEUValute onClick={this.btnEU}/>
+                <BtnUAHValute onClick={this.btnUAH}/>
+                <h3>Введи сколько тебе нужно ДОЛЛАРОВ перевести в рубли</h3>
+                <form action="">
+                    <input type="text"/>
+                </form>
+            </div>)
+        }
+        else if (inputEULoaded===false || inputUAHLoaded===true || inputUSDLoaded===false) {
+            return(<div>
+                <BtnUSDValute onClick={this.btnUSD}/>
+                <BtnEUValute onClick={this.btnEU}/>
+                <BtnUAHValute onClick={this.btnUAH}/>
+                <h3>Введи сколько тебе нужно ГРИВЕНЬ в рубли</h3>
+                <form action="">
+                    <input type="text"/>
+                </form>
+            </div>)
+        }
+
+
     }
 }
+
+
+function BtnUSDValute(props) {
+    return (
+        <div>
+            <button onClick={props.onClick}>USD</button>
+        </div>
+    )
+}
+function BtnEUValute(props) {
+    return (
+        <div>
+            <button onClick={props.onClick}>EU</button>
+        </div>
+    )
+}
+function BtnUAHValute(props) {
+    return (
+        <div>
+            <button onClick={props.onClick}>UAH</button>
+        </div>
+    )
+}
+
+// function BtnAllValute(props) {
+//     return(
+//         <div>
+//             <BtnUSDValute onClick={props.onClick}/>
+//             <BtnEUValute onClick={props.onClick}/>
+//             <BtnUAHValute onClick={props.onClick}/>
+//         </div>
+//     )
+// }
 
 export default ConverterInput;
